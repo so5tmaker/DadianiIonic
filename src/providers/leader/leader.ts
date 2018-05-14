41ef3bd48d75874b774bf2426ee5229bd509724a
Viktor Sivulskiy <svicvg@gmail.com>
2018-05-14 09:07:35 +0500

    The leader.ts File Was Updated

--- src/providers/leader/leader.ts
+++ src/providers/leader/leader.ts
@@ -1,11 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Leader } from '../../shared/leader';
+import { Observable } from 'rxjs/Observable';
+import { Http, Response } from '@angular/http';
+import { baseURL } from '../../shared/baseurl';
+import { ProcessHttpmsgProvider } from '../process-httpmsg/process-httpmsg';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class LeaderProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello LeaderProvider Provider');
+  constructor(public http: Http,
+              private processHTTPMsgService: ProcessHttpmsgProvider) { }
+
+  getLeaders(): Observable<Leader[]> {
+    return this.http.get(baseURL + 'leaders')
+                    .map(res => { return this.processHTTPMsgService.extractData(res); })
+                    .catch(error => { return this.processHTTPMsgService.handleError(error); });
+  }
+
+  getLeader(id: number): Observable<Leader> {
+    return  this.http.get(baseURL + 'leaders/'+ id)
+                    .map(res => { return this.processHTTPMsgService.extractData(res); })
+                    .catch(error => { return this.processHTTPMsgService.handleError(error); });
+  }
+
+  getFeaturedLeader(): Observable<Leader> {
+    return this.http.get(baseURL + 'leaders?featured=true')
+                    .map(res => { return this.processHTTPMsgService.extractData(res)[0]; })
+                    .catch(error => { return this.processHTTPMsgService.handleError(error); });
   }
 
 }
