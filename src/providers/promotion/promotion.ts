4ce421df151567a2c3e2f0ab0dcf06fe50162fa5
Viktor Sivulskiy <svicvg@gmail.com>
2018-05-15 09:47:27 +0500

    The promotion.ts File Was Updated

--- src/providers/promotion/promotion.ts
+++ src/providers/promotion/promotion.ts
@@ -1,11 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Promotion } from '../../shared/promotion';
+import { Observable } from 'rxjs/Observable';
+import { Http, Response } from '@angular/http';
+import { baseURL } from '../../shared/baseurl';
+import { ProcessHttpmsgProvider } from '../process-httpmsg/process-httpmsg';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class PromotionProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello PromotionProvider Provider');
+  constructor(public http: Http,
+              private processHTTPMsgService: ProcessHttpmsgProvider) { }
+
+  getPromotions(): Observable<Promotion[]> {
+    return this.http.get(baseURL + 'promotions')
+                    .map(res => { return this.processHTTPMsgService.extractData(res); })
+                    .catch(error => { return this.processHTTPMsgService.handleError(error); });
+  }
+
+  getPromotion(id: number): Observable<Promotion> {
+    return  this.http.get(baseURL + 'promotions/'+ id)
+                    .map(res => { return this.processHTTPMsgService.extractData(res); })
+                    .catch(error => { return this.processHTTPMsgService.handleError(error); });
+  }
+
+  getFeaturedPromotion(): Observable<Promotion> {
+    return this.http.get(baseURL + 'promotions?featured=true')
+                    .map(res => { return this.processHTTPMsgService.extractData(res)[0]; })
+                    .catch(error => { return this.processHTTPMsgService.handleError(error); });
   }
 
 }
