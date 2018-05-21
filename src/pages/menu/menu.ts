f8a9e9ed8c2685743f405d56bec1e1f2d1e2da72
Viktor Sivulskiy <svicvg@gmail.com>
2018-05-21 14:07:18 +0600

    The menu.ts File Was Updated

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,25 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the MenuPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Dish } from '../../shared/dish';
+import { DishProvider } from '../../providers/dish/dish';
+import { DishdetailPage } from '../dishdetail/dishdetail';
 
 @IonicPage()
 @Component({
   selector: 'page-menu',
   templateUrl: 'menu.html',
 })
-export class MenuPage {
+export class MenuPage implements OnInit {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  dishes: Dish[];
+  errMess: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private dishservice: DishProvider,
+    @Inject('BaseURL') private BaseURL) { }
+  
+  ngOnInit() {
+    this.dishservice.getDishes()
+      .subscribe(dishes => this.dishes = dishes,
+        errmess => this.errMess = <any>errmess);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MenuPage');
+  }
+
+  dishSelected(event, dish) {
+    // That's right, we're pushing to ourselves!
+    this.navCtrl.push(DishdetailPage, {
+      dish: dish
+    });
   }
 
 }
