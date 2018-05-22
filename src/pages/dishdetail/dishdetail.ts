f8c280bdf0da5f7b9e1be120540b74a026743020
Viktor Sivulskiy <svicvg@gmail.com>
2018-05-22 13:05:32 +0600

    The dishdetail.ts File Was Updated

--- src/pages/dishdetail/dishdetail.ts
+++ src/pages/dishdetail/dishdetail.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Dish } from '../../shared/dish';
+import { Comment } from '../../shared/comment';
 
 @IonicPage()
 @Component({
@@ -7,8 +9,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'dishdetail.html',
 })
 export class DishdetailPage {
+  dish: Dish;
+  errMess: string;
+  avgstars: string;
+  numcomments: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    @Inject('BaseURL') private BaseURL) {
+    this.dish = navParams.get('dish');
+    this.numcomments = this.dish.comments.length;
+    let total = 0;
+    this.dish.comments.forEach(comment => total += comment.rating );
+    this.avgstars = (total/this.numcomments).toFixed(2);
   }
 
   ionViewDidLoad() {
