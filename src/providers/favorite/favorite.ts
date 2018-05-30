c595f29ec760d7e630f34531621c4a9dbcf5e03f
Viktor Sivulskiy <svicvg@gmail.com>
2018-05-30 13:27:17 +0600

    The FavoriteProvider Was Updated

--- src/providers/favorite/favorite.ts
+++ src/providers/favorite/favorite.ts
@@ -1,22 +1,45 @@
 import { HttpModule } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { Dish } from '../../shared/dish';
+import { Observable } from 'rxjs/Observable';
+import { DishProvider } from '../dish/dish';
 
 @Injectable()
 export class FavoriteProvider {
 
   favorites: Array<any>;
 
-  constructor(public http: HttpModule) {
+  constructor(public http: HttpModule,
+    private dishservice: DishProvider) {
     console.log('Hello FavoriteProvider Provider');
     this.favorites = [];
   }
 
   addFavorite(id: number): boolean {
-    this.favorites.push(id);
+    if (!this.isFavorite(id))
+      this.favorites.push(id);
+    console.log('favorites', this.favorites);
     return true;
   }
 
   isFavorite(id: number): boolean {
-        return this.favorites.some(el => el === id);
+    return this.favorites.some(el => el === id);
+  }
+
+  getFavorites(): Observable<Dish[]> {
+    return this.dishservice.getDishes()
+      .map(dishes => dishes.filter(dish => this.favorites.some(el => el === dish.id)));
+  }
+
+  deleteFavorite(id: number): Observable<Dish[]> {
+    let index = this.favorites.indexOf(id);
+    if (index >= 0) {
+      this.favorites.splice(index, 1);
+      return this.getFavorites();
+    }
+    else {
+      console.log('Deleting non-existant favorite', id);
+      return Observable.throw('Deleting non-existant favorite' + id);
+    }
   }
 }
