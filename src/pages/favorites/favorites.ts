b4e197ef32daef5468b50419487de1c8a9d5167a
Viktor Sivulskiy <svicvg@gmail.com>
2018-05-31 13:14:21 +0600

    The favorite.ts Page File Was Updated

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit, Inject } from '@angular/core';
+import { IonicPage, NavController, NavParams, ItemSliding } from 'ionic-angular';
+import { FavoriteProvider } from '../../providers/favorite/favorite';
+import { Dish } from '../../shared/dish';
 
 @IonicPage()
 @Component({
   selector: 'page-favorites',
   templateUrl: 'favorites.html',
 })
-export class FavoritesPage {
+export class FavoritesPage implements OnInit {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  favorites: Dish[];
+  errMess: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private favoriteservice: FavoriteProvider,
+    @Inject('BaseURL') private BaseURL) {
+  }
+
+  ngOnInit() {
+    this.favoriteservice.getFavorites()
+      .subscribe(favorites => this.favorites = favorites,
+        errmess => this.errMess = errmess);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FavoritesPage');
+  }
+
+  deleteFavorite(item: ItemSliding, id: number) {
+    console.log('delete', id);
+    this.favoriteservice.deleteFavorite(id)
+      .subscribe(favorites => this.favorites = favorites,
+        errmess => this.errMess = errmess);
+    item.close();
   }
 
 }
