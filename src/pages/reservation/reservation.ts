d48af73d802993d3fe2c147fbdb46c9187bd48c8
Viktor Sivulskiy <svicvg@gmail.com>
2018-06-04 13:28:16 +0600

    The reservation.ts Was Updated

--- src/pages/reservation/reservation.ts
+++ src/pages/reservation/reservation.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ReservationPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -15,11 +8,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ReservationPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public viewCtrl: ViewController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ReservationPage');
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
   }
 
 }
