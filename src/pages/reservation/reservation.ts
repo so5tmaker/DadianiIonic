bc819865b9505388c348a284e01c569d2f6cac9c
Viktor Sivulskiy <svicvg@gmail.com>
2018-06-05 13:04:37 +0600

    A Reactive Form Was Created

--- src/pages/reservation/reservation.ts
+++ src/pages/reservation/reservation.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -8,8 +9,17 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class ReservationPage {
 
+  reservation: FormGroup;
+
   constructor(public navCtrl: NavController, public navParams: NavParams,
-    public viewCtrl: ViewController) {
+    public viewCtrl: ViewController,
+    private formBuilder: FormBuilder) {
+
+    this.reservation = this.formBuilder.group({
+      guests: 3,
+      smoking: false,
+      dateTime: ['', Validators.required],
+    });
   }
 
   ionViewDidLoad() {
@@ -17,6 +27,11 @@ export class ReservationPage {
   }
 
   dismiss() {
+    this.viewCtrl.dismiss();
+  }
+
+  onSubmit() {
+    console.log(this.reservation.value);
     this.viewCtrl.dismiss();
   }
 
