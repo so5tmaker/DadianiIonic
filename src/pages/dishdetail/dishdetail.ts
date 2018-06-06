8b35ea7bac89f3c5a6821b24480b2ef75e387a7b
Viktor Sivulskiy <svicvg@gmail.com>
2018-06-06 13:00:32 +0600

    A Toast Was Added Into dishdetail.ts

--- src/pages/dishdetail/dishdetail.ts
+++ src/pages/dishdetail/dishdetail.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { Comment } from '../../shared/comment';
 
@@ -19,14 +19,19 @@ export class DishdetailPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     @Inject('BaseURL') private BaseURL,
-    private favoriteservice: FavoriteProvider
-  ) {
+    private favoriteservice: FavoriteProvider,
+    private toastCtrl: ToastController) {
     this.dish = navParams.get('dish');
     this.favorite = favoriteservice.isFavorite(this.dish.id);
     this.numcomments = this.dish.comments.length;
     let total = 0;
     this.dish.comments.forEach(comment => total += comment.rating);
     this.avgstars = (total / this.numcomments).toFixed(2);
+    this.toastCtrl.create({
+      message: 'Блюдо ' + this.dish.id + ' успешно добавлено в список избранных ',
+      position: 'middle',
+      duration: 3000
+    }).present();
   }
 
   ionViewDidLoad() {
