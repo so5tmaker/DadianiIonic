d3e24e462d8ee91c5164b95937a29b44c2c4bbe8
Viktor Sivulskiy <svicvg@gmail.com>
2018-06-07 13:13:48 +0600

    An Action Sheet Was Added Into dishdetail.ts

--- src/pages/dishdetail/dishdetail.ts
+++ src/pages/dishdetail/dishdetail.ts
@@ -1,5 +1,8 @@
 import { Component, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import {
+  IonicPage, NavController, NavParams, ToastController,
+  ActionSheetController, ModalController
+} from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { Comment } from '../../shared/comment';
 
@@ -18,20 +21,18 @@ export class DishdetailPage {
   favorite: boolean;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-    @Inject('BaseURL') private BaseURL,
+    public actionSheetCtrl: ActionSheetController,
     private favoriteservice: FavoriteProvider,
-    private toastCtrl: ToastController) {
+    private toastCtrl: ToastController,
+    public modalCtrl: ModalController,
+    @Inject('BaseURL') private BaseURL
+  ) {
     this.dish = navParams.get('dish');
     this.favorite = favoriteservice.isFavorite(this.dish.id);
     this.numcomments = this.dish.comments.length;
     let total = 0;
     this.dish.comments.forEach(comment => total += comment.rating);
     this.avgstars = (total / this.numcomments).toFixed(2);
-    this.toastCtrl.create({
-      message: 'Блюдо ' + this.dish.id + ' успешно добавлено в список избранных ',
-      position: 'middle',
-      duration: 3000
-    }).present();
   }
 
   ionViewDidLoad() {
@@ -41,6 +42,39 @@ export class DishdetailPage {
   addToFavorites() {
     console.log('Adding to Favorites', this.dish.id);
     this.favorite = this.favoriteservice.addFavorite(this.dish.id);
+    this.toastCtrl.create({
+      message: 'Блюдо ' + this.dish.id + ' успешно добавлено в список избранных ',
+      position: 'middle',
+      duration: 3000
+    }).present();
+  }
+
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Выберите действие',
+      buttons: [
+        {
+          text: 'Добавить в избранное',
+          handler: () => {
+            this.addToFavorites();
+            console.log('Add To Favorites clicked');
+          }
+        }, {
+          text: 'Добавить комментарий',
+          handler: () => {
+            //this.openComment();
+            console.log('Add Comment clicked');
+          }
+        }, {
+          text: 'Отменить',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
 }
