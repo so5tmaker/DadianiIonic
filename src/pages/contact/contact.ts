5fb03e16bb1b34959107fb129d5ffa9b3a2947bb
Viktor Sivulskiy <svicvg@gmail.com>
2018-06-29 13:38:39 +0600

    The contact.ts Was Updated To Send Emails

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -10,11 +10,25 @@ import { EmailComposer } from '@ionic-native/email-composer';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private emailComposer: EmailComposer) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ContactPage');
+  }
+
+  sendEmail() {
+
+    let email = {
+      to: 'confusion@food.net',
+      subject: '[ConFusion]: Query',
+      body: 'Dear Sir/Madam:',
+      isHtml: true
+    };
+
+    // Send a text message using default options
+    this.emailComposer.open(email);
   }
 
 }
