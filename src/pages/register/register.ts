6b4c3641c4cf7f3263d4969b1cb229423caadb35
Viktor Sivulskiy <svicvg@gmail.com>
2018-07-11 07:13:42 +0600

    The Functions dismiss() And getPicture() Were Added Into register.ts

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
-import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @IonicPage()
@@ -18,21 +18,46 @@ export class RegisterPage {
     private camera: Camera,
     private formBuilder: FormBuilder) {
 
-      this.registerForm = this.formBuilder.group({
-        firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
-        lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
-        username: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(25)] ],
-        password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(25)] ],
-        telnum: ['', [Validators.required, Validators.pattern] ],
-        email: ['', [Validators.required, Validators.email] ],
-      });
-      
+    this.registerForm = this.formBuilder.group({
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      username: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(25)]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(25)]],
+      telnum: ['', [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
+    });
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
   }
 
-  // 3
+  dismiss() {
+    this.viewCtrl.dismiss(true);
+  }
+
+  getPicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      targetHeight: 100,
+      targetWidth: 100,
+      correctOrientation: true,
+      allowEdit: true,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE,
+      cameraDirection: this.camera.Direction.FRONT
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+
+      this.image = imageData;
+      console.log(imageData);
+    }, (err) => {
+      console.log('Error obtaining picture')
+    });
+  }
+
 
 }
