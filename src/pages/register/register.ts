f18f248a1c6ea4e070225d9b70d3d44775f39928
Viktor Sivulskiy <svicvg@gmail.com>
2018-07-11 07:12:04 +0600

    The Registration Form Was Built

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-// 1
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-// 
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
@@ -10,10 +10,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegisterPage {
 
-  // 2
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  registerForm: FormGroup;
+  image: string = 'assets/images/logo.png';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private viewCtrl: ViewController,
+    private camera: Camera,
+    private formBuilder: FormBuilder) {
+
+      this.registerForm = this.formBuilder.group({
+        firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+        lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+        username: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(25)] ],
+        password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(25)] ],
+        telnum: ['', [Validators.required, Validators.pattern] ],
+        email: ['', [Validators.required, Validators.email] ],
+      });
+      
   }
- //
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
   }
