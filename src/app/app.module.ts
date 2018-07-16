3662f91ee1931ee480d34edaedd3b0c3fa1ee496
Viktor Sivulskiy <svicvg@gmail.com>
2018-07-16 08:35:06 +0600

    The app.module.ts Was Updated To Import The Network Information Plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,7 @@ import { LocalNotifications } from '@ionic-native/local-notifications';
 import { EmailComposer } from '@ionic-native/email-composer';
 import { SocialSharing } from '@ionic-native/social-sharing';
 import { Camera } from '@ionic-native/camera';
-
+import { Network } from '@ionic-native/network';
 
 @NgModule({
   declarations: [
@@ -81,7 +81,8 @@ import { Camera } from '@ionic-native/camera';
     LocalNotifications,
     EmailComposer,
     SocialSharing,
-    Camera
+    Camera,
+    Network
   ]
 })
 export class AppModule {}
