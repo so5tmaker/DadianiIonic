c015efc11306f34dce2f8628ad755397c51604fc
Viktor Sivulskiy <svicvg@gmail.com>
2018-07-18 07:03:00 +0600

    The contact.ts Was Updated To Import The Call Number Plugin

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { EmailComposer } from '@ionic-native/email-composer';
-// 1
+import { CallNumber } from '@ionic-native/call-number';
 
 @IonicPage()
 @Component({
